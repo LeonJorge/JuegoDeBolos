9e37816ffa91703bfdb599023766eb41ce0962c4
LeonJorge <jorgerodrigoleonrgit@gmail.com>
2011-08-29 19:19:08 -0400

    Tercera Prueba

--- src/JuegoDeBolos.java
+++ src/JuegoDeBolos.java
@@ -2,20 +2,29 @@
 public class JuegoDeBolos {
 	private int[] lanzamientos = new int[21];
 	int score=0;
+	int casilleroActual=0;
 	public JuegoDeBolos()
 	{
 		for(int i=0;i<21;i++)
 			lanzamientos[i]=0;
 	}
 	public void realizarLanzamiento(int pinos) {
-			lanzamientos[0]=pinos;
+			lanzamientos[casilleroActual]=pinos;
+			casilleroActual++;
+			score=score+pinos;
 	}
 	public int calcularScore()
 	{
-		for(int casillero=0;casillero<21;casillero++)
+		int totalScore=0;
+		int casillaCentral=0;
+		for(int casillero=0;casillero<10;casillero++)
 		{
-			score=score+lanzamientos[casillero];
+			totalScore=totalScore+sumaDeLosLanzamientos(casillaCentral);
+			casillaCentral+=2;
 		}
-		return score;
+		return totalScore;
+	}
+	private int sumaDeLosLanzamientos(int casillaCentral){
+		return lanzamientos[casillaCentral]+lanzamientos[casillaCentral+1];
 	}
 }
--- src/JuegoDeBolosTest.java
+++ src/JuegoDeBolosTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 public class JuegoDeBolosTest {
 	public JuegoDeBolos juego = new JuegoDeBolos();	
 	@Test
-	public void elPuntajeDeberiaSerCeroSiElPrimerDerribaCeroPinos(){
+	public void elScoreDeberiaSerCeroSiElPrimerDerribaCeroPinos(){
 		int pinos=0;
 		juego.realizarLanzamiento(pinos);
 		assertEquals(juego.calcularScore(),0);
 	}
 	@Test
-	public void elPuntajeDeberiaSerCeroSiTodosLosLanzamientosSonFallados(){
+	public void elScoreDeberiaSerCeroSiTodosLosLanzamientosSonFallados(){
 		realizarNLanzamientos(0,20);
 		assertEquals(0,juego.calcularScore());
 	}
@@ -19,6 +19,11 @@ public class JuegoDeBolosTest {
 		for(int lanzamiento=0;lanzamiento<veces;lanzamiento++){
 			juego.realizarLanzamiento(pinos);
 		}
+	}
+	@Test
+	public void elScoreDeberiaSer20SiTodosLosLanzamientosDerribanUno(){
+		realizarNLanzamientos(1,20);
+		assertEquals(20,juego.calcularScore());
 	}
 }
 	
